b5ce4985a87a6d2f1c7a8700d1250d79b5b8bd53
명세교 <myungsekyo@gmail.com>
2017-12-29 23:49:19 +0900

    RecyclerView added

---
+++ app/src/main/java/com/segyo/memo/RecyclerViewItem.java
@@ -0,0 +1,26 @@
+package com.segyo.memo;
+
+/**
+ * Created by sekyo on 2017-12-29.
+ */
+
+public class RecyclerViewItem {
+    private String content = null;
+    private String time = null;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
